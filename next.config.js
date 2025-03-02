86d159678b87f46b7787fc27c80e21c062a31fc9
imnarasa <pediredlanarasaraju@gmail.com>
2025-03-02 22:32:10 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+}

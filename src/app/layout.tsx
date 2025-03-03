213f7b3229ae147e38a939003ee8ae87c07f3322
imnarasa <pediredlanarasaraju@gmail.com>
2025-03-03 16:09:24 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Portfolio',
-  description: 'Frontend Developer & UI Designer Portfolio',
+  title: 'Narasaraj',
+  description: 'Designer Portfolio',
 }
 
 export default function RootLayout({
